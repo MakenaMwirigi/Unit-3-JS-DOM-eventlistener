1ed7433fb3fa1c3d1822c258b80c1883132b425e
MakenaMwirigi <makenasharon46@gmail.com>
2025-05-20 12:01:46 +0300

    event listener assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -10,3 +10,16 @@ describe("", () => {
     });
   });
 });
+
+function addingEventListener() {
+  const input = document.getElementById('button');
+
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+
+  input.addEventListener('click', clickAlert);
+}
+
+// Call the function to activate it when index.js loads
+addingEventListener();
